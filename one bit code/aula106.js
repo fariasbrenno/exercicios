d029a280768a20eb19e494c42ec23df2e74860cb
Brenno Farias <brenno.developer@gmail.com>
2022-10-04 16:44:44 -0300

    Salvando versão atual do js

--- one bit code/aula106.js
+++ one bit code/aula106.js
@@ -3,16 +3,31 @@ let btnStart = document.getElementById('iniciar');
 function iniciar (){
     let velocity = 0;
     let naveName = prompt("Qual o nome da nave?");
+
     let option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
 
-    if((option !== 1) || (option !== 2) || (option !== 3) || (option !== 4)){
-        alert("Opção inválida!");
+    while((option !== 1) || (option !== 2) || (option !== 3) || (option !== 4)){
+
+        do{
+            switch (option){
+                case '1':
+                    alert('Soma');
+                    break;
+                case '2':
+                    alert('Subtração');
+                    break;
+                case '3':
+                    alert('Op 3');
+                    break;
+                case '4':
+                    alert('Op 4');
+                    break;
+
+            let option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
+            }
+        } while((option !== 1) || (option !== 2) || (option !== 3) || (option !== 4))
         
-        if(option == 1){
-            alert("Acelerar");
-        }
     }
-    
 }
 
 
