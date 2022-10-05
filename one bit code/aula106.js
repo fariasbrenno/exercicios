62a56f29279bd306bec0d5c2b7699b13727bb327
Brenno Farias <brenno.developer@gmail.com>
2022-10-05 17:02:48 -0300

    Mudando js

--- one bit code/aula106.js
+++ one bit code/aula106.js
@@ -1,35 +1,24 @@
-let btnStart = document.getElementById('iniciar');
-
 function iniciar (){
     let velocity = 0;
     let naveName = prompt("Qual o nome da nave?");
+    let changeOption;
 
-    let option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
+    function menu(){
+        let option;
 
-    while((option !== 1) || (option !== 2) || (option !== 3) || (option !== 4)){
+        while(option!=1 && option!=2 && option!=3 && option!=4){
+            option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
+        }
 
-        do{
-            switch (option){
-                case '1':
-                    alert('Soma');
-                    break;
-                case '2':
-                    alert('Subtração');
-                    break;
-                case '3':
-                    alert('Op 3');
-                    break;
-                case '4':
-                    alert('Op 4');
-                    break;
+        return option;
+    }
 
-            let option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
-            }
-        } while((option !== 1) || (option !== 2) || (option !== 3) || (option !== 4))
+    function speed(){
         
     }
 }
 
 
+let btnStart = document.getElementById('iniciar');
 //Eventos
 btnStart.addEventListener('click', iniciar);
