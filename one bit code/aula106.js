7a5b6d984a8358e0d91c0d15334186ba84f23ce4
Brenno Farias <brenno.developer@gmail.com>
2022-10-17 16:04:36 -0300

    Criando função de menu

--- one bit code/aula106.js
+++ one bit code/aula106.js
@@ -1,7 +1,18 @@
 function iniciar(){
     let spaceVelocity = 0;
-    let spaceName;
+    let spaceName = prompt ("Qual o nome da nave?")
+    let spaceOption;
+
+    function menu(){
+        let menuOption;
+        while(menuOption != 1 && menuOption != 2 && menuOption != 3 && menuOption != 4){
+            menuOption = prompt(`Olá, ${spaceName}\nEscolha uma opção\n1-Acelerar a nave em 5km/s\n2-Desacelerar a nave em 5km/s\n3-Imprimir informações da nave\n4-Sair do programa`);
+        }
+        return menuOption;
+    }
+    
 }
 
+//Capturando botão do HTML e adicionando função para iniciar o programa
 let btnStart = document.getElementById("iniciar");
 btnStart.addEventListener('click', iniciar);
