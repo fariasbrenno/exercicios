71678b225ba50716bcd9b5b37770450c594ddd04
Brenno Farias <brenno.developer@gmail.com>
2022-10-17 15:33:28 -0300

    Algoritimo concluído

--- one bit code/aula106.js
+++ one bit code/aula106.js
@@ -7,13 +7,43 @@ function iniciar (){
         let option;
 
         while(option!=1 && option!=2 && option!=3 && option!=4){
-            option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerr a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
+            option = prompt (`Olá, ${naveName}\nEscolha uma das opções:\n1-Acelerar a nave em 5km/s\n2-Desacelerar a nave em 5km/s\n3-Imprimir dados de bordo\n4-sair do programa`);
         }
 
         return option;
     }
 
-    
+    function speedUp(velocity){
+        let newVelocity = velocity + 5;
+        return newVelocity;
+    }
+    function slowDown(velocity){
+        let newVelocity = velocity - 5;
+        if(newVelocity < 0){
+            newVelocity = 0;
+        }
+        return newVelocity;
+    }
+    function printData(name, velocity){
+        alert("Espaçonave: "+name+"\nVevolidade: "+velocity+" km/s");
+    }
+
+    do{
+        changeOption = menu();
+        switch(changeOption){
+            case "1":
+                velocity = speedUp(velocity);
+                break
+            case "2":
+                velocity = slowDown(velocity);
+                break
+            case "3":
+                printData(naveName, velocity);
+                break
+            case "4":
+                alert("Programa encerrado!");
+            }
+    }while(changeOption != 4);
 }
 
 
