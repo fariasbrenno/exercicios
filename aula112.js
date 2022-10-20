a9d3f0a3c4b08171486fb3b0fc5bcfe7943cf811
Brenno Farias <brenno.developer@gmail.com>
2022-10-20 17:30:06 -0300

    Iniciando exercício da aula 112 callback

---
+++ aula112.js
@@ -0,0 +1,24 @@
+function iniciarPrograma (){
+    let initialVelocity = 150;
+
+    function slowDown () {
+
+        while(initialVelocity > 0){
+            document.write(initialVelocity+"<br>");
+            initialVelocity = initialVelocity - 20;
+    
+            if(initialVelocity < 0){
+                initialVelocity = 0;
+                document.write(initialVelocity);
+            }
+        }
+    }
+    function printVelocity (slow){
+        document.write(slow+"<br>");
+    }
+ 
+}
+
+//Eventos
+let btnStart = document.getElementById("btn-iniciar");
+btnStart.addEventListener('click', iniciarPrograma);
