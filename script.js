a8f0aca3a25482fef9c56ff0d8e74f6b238127c3
Brenno Farias <brenno.developer@gmail.com>
2022-10-20 16:16:06 -0300

    Testando função callback

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let velocidade = function (velocity){
 
 let newVelocity = velocidade (40);
 console.log (newVelocity);
+
+function print(num){
+    console.log("Resultado função callback "+num);
+}
+function soma(a, b, cb){
+    let op = a+b;
+    cb(op);
+}
+
+soma(5,5,print);
