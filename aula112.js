441dabaf1fb7dde4fbec7fdb07d6ce9496480150
Brenno Farias <brenno.developer@gmail.com>
2022-10-24 10:51:48 -0300

    Exercício aula 112 concluída

--- aula112.js
+++ aula112.js
@@ -2,22 +2,31 @@ function iniciarPrograma (){
     let initialVelocity = 150;
 
     function slowDown () {
+        printVelocity(initialVelocity);
 
         while(initialVelocity > 0){
-            document.write(initialVelocity+"<br>");
+            
             initialVelocity = initialVelocity - 20;
     
             if(initialVelocity < 0){
                 initialVelocity = 0;
-                document.write(initialVelocity);
+            }
+
+            printVelocity(initialVelocity);
+
+            if (initialVelocity == 0) {
+                document.write("Nave parada: As comportas podem ser abertas.");
             }
         }
     }
     
     function printVelocity (slow){
-        document.write(slow+"<br>");
+        document.write("Velocidade atual "+slow+"km/s <br>");
     }
- 
+
+    slowDown();
+     
+
 }
 
 //Eventos
